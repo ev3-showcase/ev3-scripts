9e7507fcb5e44d1aba81b21629583e4d33c99b50
Felix Knoepke <morytox@hotmail.de>
2019-04-03 17:52:12 +0200

    adds utf-8 decoding for mqtt message strings and changes placeholder for hostname in connectionstrings.

--- subscriber.py
+++ subscriber.py
@@ -7,12 +7,12 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("test/#")
 
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    print(msg.topic + " " + msg.payload.decode('utf-8'))
 
 client = mqtt.Client("subscriber",transport='websockets')
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("dasd", 9001, 60)
+client.connect("<hostname>", 9001, 60)
 
 client.loop_forever()
