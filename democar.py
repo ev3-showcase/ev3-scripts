8977b608054f8f6cb259c28d87a474c426ad0c51
Michael Rose <michaelrose.1992@gmail.com>
2019-04-12 20:22:31 +0200

    atleast the encapsulation works

--- democar.py
+++ democar.py
@@ -9,17 +9,34 @@ from distutils.util import strtobool
 
 import paho.mqtt.client as mqtt
 
-class Car:
-    def __init__(self, name, mqtt_client, speed=0, steering=0, min_speed=-90, max_speed=90,min_steer=-90, max_steer=90):
-        self.name = name
+class Car(mqtt.Client):
+    def __init__(self, car_name=('car-' + uuid.uuid4().hex.upper()[0:6]), speed=0, steering=0, min_speed=-90, max_speed=90,min_steer=-90, max_steer=90, broker='localhost', port=1883):
+        self.name = car_name
         self.speed = speed
         self.steering = steering
         self.MIN_SPEED = min_speed
         self.MAX_SPEED = max_speed
         self.MIN_STEER = min_steer
         self.MAX_STEER = max_steer
-        self.mqtt_client.on_connect = self.on_connect
-        self.mqtt_client.on_message = self.on_message
+        self.broker = broker
+        self.port = port
+
+        super().__init__(self.name) # actually create the client and call it like the car
+        self.on_connect = on_connect
+
+        self.connect(broker, port, 60) # connect to the broker
+
+    def on_connect(self, userdata, flags, rc):
+        if rc==0:
+            self.connected_flag=True
+            print("connected OK Returned code=",rc)
+            self.subscribe('car/speed')
+        else:
+            print("Bad connection Returned code= ",rc)
+
+    def on_message(client, userdata, msg):
+        print("Got a message")
+        self.accelerate(int(msg.payload))
 
     def __repr__(self):
         return("Car %s" % self.name)
@@ -39,18 +56,6 @@ class Car:
         else:
             raise ValueError('Value is out of range MIN_STEER, MAX_STEER')
 
-    def on_connect(client, userdata, flags, rc):
-        print(topics)
-        if rc==0:
-            client.connected_flag=True
-            print("connected OK Returned code=",rc)
-            client.subscribe('car/speed')
-        else:
-            print("Bad connection Returned code= ",rc)
-
-    def on_message(client, userdata, msg):
-        print("Got a message")
-        self.accelerate(int(msg.payload))
 
 def sigterm_handler(signal, frame):
     client.loop_stop(Force=True)
@@ -61,33 +66,14 @@ def sigterm_handler(signal, frame):
 def main():
     signal.signal(signal.SIGTERM, sigterm_handler)
     
-    broker = os.getenv('MQTT_BROKER', 'localhost')
-    port =  int(os.getenv('MQTT_PORT', 1883))
-    sub_name = os.getenv('HOSTNAME', ('car-' + uuid.uuid4().hex.upper()[0:6]))
-    websocket = strtobool(os.getenv('MQTT_SOCKET', 'False'))
-    
-    if websocket:
-        print('Connecting to %s:%d as %s via websocket' % (broker, port, sub_name))
-        client = mqtt.Client(sub_name,transport='websockets')
-    else:
-        print('Connecting to %s:%d as %s' % (broker, port, sub_name))
-        client = mqtt.Client(sub_name)
-   
 #    client.on_connect = on_connect
 #    client.on_message = on_message
 
-    global topics
-    global lego
-   
-    lego = Car('legocar', client)
-    
-    topics = ['car/speed', 'car/steering']
-    
-    client.connect(broker, port, 60)
-    client.loop_start()
-    while(True):
-        print("A Car is driving %d speed and steering %d" % (lego.speed, lego.steering), end='\r')
-    client.loop_stop()
+    broker = os.getenv('MQTT_BROKER', 'localhost')
+    port =  int(os.getenv('MQTT_PORT', 1883))
+
+    lego = Car('legocar', broker=broker, port=port)
+    lego.subscribe(('car/speed',0),('car/steering',0))
 
 if __name__ == '__main__':
     main()
